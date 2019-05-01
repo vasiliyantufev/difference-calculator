d91ccb2fd92f7ff6aa2ab613f4b065ea258278f8
Vasiliy Antufev <vasiliyantufev@gmail.com>
2019-05-01 23:15:56 +0300

    gendiff fix

--- src/Differ.php
+++ src/Differ.php
@@ -4,14 +4,18 @@ namespace DifferenceCalculator;
 
 use Funct;
 
-function diff($pathToFile1, $pathToFile2)
+function diff($fmt, $pathToFile1, $pathToFile2)
 {
+
+    var_dump($fmt);
+    var_dump($pathToFile1);
+    var_dump($pathToFile2);
+
     //Funct\Strings\classify('hello world');
 
     //загрузить два файла в массивы
-    $handlerFile1 = fopen($pathToFile1, "rb");
-    $handlerFile2 = fopen($pathToFile2, "rb");
-
+//    $handlerFile1 = fopen($pathToFile1, "rb");
+//    $handlerFile2 = fopen($pathToFile2, "rb");
     //найти различия
     //записать в строку и вернуть
 }
