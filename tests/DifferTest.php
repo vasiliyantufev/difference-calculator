44683db1592645da9263625ab5162c517d170446
Vasiliy Antufev <vasiliyantufev@gmail.com>
2019-07-01 18:27:01 +0300

    DOC

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -4,9 +4,47 @@ namespace Differ\tests;
 
 use \PHPUnit\Framework\TestCase;
 
+$pretty = <<<DOC
+{
+    common: {
+        setting1: Value 1
+      - setting2: 200
+        setting3: true
+      - setting6: {
+            key: value
+        }
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+    }
+    group1: {
+      + baz: bars
+      - baz: bas
+        foo: bar
+    }
+  - group2: {
+        abc: 12345
+    }
+  + group3: {
+        fee: 100500
+    }
+}
+DOC;
+
+$plain = <<<DOC
+Property 'common.setting2' was removed
+Property 'common.setting6' was removed
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: 'complex value'
+Property 'group1.baz' was changed. From 'bas' to 'bars'
+Property 'group2' was removed
+Property 'group3' was added with value: 'complex value'
+DOC;
 
 class DifferTest extends TestCase
 {
+
     public function testNotLol()
     {
         $this->assertNotEquals('lol','lol2');
