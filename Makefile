1e8daf1d23e56f579317e315d2f86838e8a84496
Vasiliy Antufev <vasiliyantufev@gmail.com>
2019-07-04 10:16:52 +0300

    travis fix2

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 install:
 	composer install
 lint:
-	composer run-script phpcs -- --standard=PSR12 src bin
+	composer run-script phpcs -- --standard=PSR12 src bin tests
 test:
 	composer run-script phpunit tests
--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -9,9 +9,9 @@ use \PHPUnit\Framework\TestCase;
 class DifferTest extends TestCase
 {
 
-const PATH_FILES = 'files' . DIRECTORY_SEPARATOR;
+    const PATH_FILES = 'files' . DIRECTORY_SEPARATOR;
 
-const PRETTY = <<<DOC
+    const PRETTY = <<<DOC
 {
     common: {
         setting1: Value 1
@@ -39,7 +39,7 @@ const PRETTY = <<<DOC
 }
 DOC;
 
-const PLAIN = <<<DOC
+    const PLAIN = <<<DOC
 Property 'common.setting2' was removed
 Property 'common.setting6' was removed
 Property 'common.setting4' was added with value: 'blah blah'
@@ -49,27 +49,30 @@ Property 'group2' was removed
 Property 'group3' was added with value: 'complex value'
 DOC;
 
-    private function  getPath(string $file)
-    {
-        return self::PATH_FILES.$file;
-    }
+//    private function  getPath(string $file)
+//    {
+//        return self::PATH_FILES.$file;
+//    }
 
 //    public function testPretty()
 //    {
-//        $this->assertEquals(self::PRETTY, diff('pretty', $this->getPath('testBefore.json'),$this->getPath('testAfter.json')));
+//        $this->assertEquals(self::PRETTY, diff('pretty', $this->getPath('testBefore.json'),
+//              $this->getPath('testAfter.json')));
 //    }
 
 //    public function testPlain()
 //    {
-//        $this->assertEquals(self::PLAIN, diff('plain', $this->getPath('testBefore.json'),$this->getPath('testAfter.json')));
+//        $this->assertEquals(self::PLAIN, diff('plain', $this->getPath('testBefore.json'),
+//              $this->getPath('testAfter.json')));
 //    }
 
     public function testNotLol()
     {
-        $this->assertNotEquals('lol','lol2');
+        $this->assertNotEquals('lol', 'lol2');
     }
+
     public function testLol()
     {
-        $this->assertEquals('lol','lol');
+        $this->assertEquals('lol', 'lol');
     }
 }
